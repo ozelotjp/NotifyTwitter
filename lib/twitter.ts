2c69678bfcad285a3549aba061164a317cbaed2d
ozelotjp <contact@ozelot.dev>
2021-12-31 14:05:52 +0000

    feat: Twitter Tokenを利用する方式に変更

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -1,35 +1,15 @@
 import { encode } from "https://deno.land/std/encoding/base64.ts";
 import { env } from "./env.ts";
 
-const CONSUMER_KEY = env("TWITTER_KEY");
-const CONSUMER_SECRET = env("TWITTER_SECRET");
+const TWITTER_TOKEN = env("TWITTER_TOKEN");
 
-const getToken = async () => {
-  const response = await fetch(`https://api.twitter.com/oauth2/token`, {
-    method: "POST",
-    headers: {
-      "Authorization": `Basic ${encode(`${CONSUMER_KEY}:${CONSUMER_SECRET}`)}`,
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-    },
-    body: "grant_type=client_credentials",
-  });
-
-  if (!response.ok) {
-    console.error(response);
-    throw new Error("Authorization Error");
-  }
-
-  const result = await response.json();
-  return result.access_token;
-};
-
-const getUsersShow = async (token: string, screen_name: string) => {
+const getUsersShow = async (screen_name: string) => {
   const res = await fetch(
     `https://api.twitter.com/1.1/users/show.json?screen_name=${screen_name}`,
     {
       method: "GET",
       headers: {
-        "Authorization": `Bearer ${token}`,
+        "Authorization": `Bearer ${TWITTER_TOKEN}`,
       },
     },
   );
@@ -44,8 +24,7 @@ const getUsersShow = async (token: string, screen_name: string) => {
 };
 
 export const getProfile = async (screen_name: string) => {
-  const token = await getToken();
-  const data = await getUsersShow(token, screen_name);
+  const data = await getUsersShow(screen_name);
 
   return {
     name: data.name,
